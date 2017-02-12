eb6acc964741674742a82ec987d85528bc10585f
angular-cli <angular-cli@angular.io>
2017-02-12 22:58:21 +0800

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MoeOJFrontEndNGPage } from './app.po';
+
+describe('moe-oj-front-end-ng App', function() {
+  let page: MoeOJFrontEndNGPage;
+
+  beforeEach(() => {
+    page = new MoeOJFrontEndNGPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
